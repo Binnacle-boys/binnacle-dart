afa47fc256ba3449ff3821a905105540ef63a0fa
William Walker <wnwalker@ucsc.edu>
2019-02-07 16:34:38 -0800

    Added a basic class to provide an abstracted data model.

---
+++ binnacle/lib/DataModel.dart
@@ -0,0 +1,15 @@
+class DataModel{
+  double windSpeed;
+  double windDirection;
+  double idealHeading;
+  double idealBoomPosition;
+  double compassHeading;
+
+  DataModel(){
+    windSpeed = 0;
+    windDirection = 0;
+    idealHeading = 0;
+    idealBoomPosition = 0;
+    compassHeading = 0;
+  }
+}

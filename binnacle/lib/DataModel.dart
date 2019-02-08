db621089b47d6a032ebe2061536b35a35a86147f
William Walker <wnwalker@ucsc.edu>
2019-02-07 16:39:36 -0800

    Added boatSpeed field to data model for speed from GPS smartphone module

--- binnacle/lib/DataModel.dart
+++ binnacle/lib/DataModel.dart
@@ -1,6 +1,7 @@
 class DataModel{
   double windSpeed;
   double windDirection;
+  double boatSpeed;
   double idealHeading;
   double idealBoomPosition;
   double compassHeading;
@@ -8,6 +9,7 @@ class DataModel{
   DataModel(){
     windSpeed = 0;
     windDirection = 0;
+    boatSpeed = 0;
     idealHeading = 0;
     idealBoomPosition = 0;
     compassHeading = 0;

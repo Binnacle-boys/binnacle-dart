83e3f7b784232af759b3f99ac6b9b25643a77fbc
nkalscheuer <nkalscheuer@gmail.com>
2019-02-10 17:14:09 -0800

    Making values streams in boat class

--- binnacle/lib/DataModel.dart
+++ binnacle/lib/DataModel.dart
@@ -18,14 +18,11 @@ abstract class Boat {
   /// The position of the boom measured in degrees. This angle will always
   /// be with in a range of [90, 270] degrees since the boom should not rotate
   /// all the way around. Relative to the sailboat.
-  double _boomAngle;
-  double get boomAngle => _boomAngle;
+  Stream<double> _boomAngle;
+  Stream<double> get boomAngle => _boomAngle;
 
   /// Position of the boat which includes, course heading,
-  /// ground speed, and location (lat/long). 
-  Position _position;
-  Position get position => _position;
-
+  /// ground speed, and location (lat/long).
   // Position stream, for listening on the positions values
   Stream<Position> _positionStream;
   Stream<Position> get positionStream => _positionStream;
@@ -34,13 +31,13 @@ abstract class Boat {
   /// vs current course. This important to have two differing heading
   /// components to distinguish boats direction vs movement direction 
   /// which could include currents.
-  double _compassHeading;
-  double get compassHeading => _compassHeading;
+  Stream<double> _compassHeading;
+  Stream<double> get compassHeading => _compassHeading;
 
   /// Angle of list, the angle that the boat is in the water. 0 means the boat
   /// is completely flat, where 180 means it's capsized perfectly.
-  double _listAngle;
-  double get listAngle => _listAngle;
+  Stream<double> _listAngle;
+  Stream<double> get listAngle => _listAngle;
 
   /// DateTime when these values were last updated.
   DateTime _lastUpdate;

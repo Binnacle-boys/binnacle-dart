28f710f47ba0c7015368b72801e6ffb5fc11c5ff
nkalscheuer <nkalscheuer@gmail.com>
2019-02-17 17:20:27 -0800

    Setting up

--- binnacle/test/SpeedWidget_test.dart
+++ binnacle/test/SpeedWidget_test.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sos/SpeedWidget.dart';
-
+import 'package:mockito/mockito.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:geolocator/geolocator.dart';
 
 
 void main() {
@@ -12,10 +14,25 @@ void main() {
   );
   testWidgets("Testing position stream", 
     (WidgetTester tester) async {
-      expect(true, true);
+      final positionSubject = new BehaviorSubject<Position>();
+      await tester.pumpWidget(SpeedWidget(positionStream: positionSubject.stream));
+      positionSubject.stream.listen((Position pos) {
+        print("Speed:" + pos.speed.toString());
+      });
+      expect(find.text("Loading speed"), findsOneWidget);
+      //Making mock position of speed 0 for testing
+      Position mockPosition = new Position(speed: 0.0);
+      
+      //Update stream
+      positionSubject.add(mockPosition);
+
+      // Waiting for the listen emission
+      await tester.pump(Duration.zero);
+      expect(find.text("0.0 mph"), findsOneWidget);
       /// TODO: Do pull request on the geolocator plug-in to add a 
       /// public constructor for simpler testing. Not able to mock 
       /// position at the moment.
+      
     }
   );
 }

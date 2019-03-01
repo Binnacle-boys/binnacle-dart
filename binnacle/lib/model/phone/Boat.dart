4b886b6662510072e9f550fa07f4a87e5da50d15
William Walker <wnwalker@ucsc.edu>
2019-02-28 16:27:13 -0800

    Continued DataModel refactor.
    --Updated DataModel to use Singleton pattern where necessary
    --Fixed DataModel test

--- binnacle/lib/model/phone/Boat.dart
+++ binnacle/lib/model/phone/Boat.dart
@@ -4,9 +4,14 @@ import '../Boat.dart';
 /// Interface for the data collected from a sailboat. Implemented for both
 /// the live sailboat and a mocked "ideal" sailboat (generated from algorithm).
 class PhoneBoat extends Boat {
+  static final PhoneBoat instance = new PhoneBoat._internal();
   Geolocator geolocator;
 
-  PhoneBoat() {
+  factory PhoneBoat() {
+    return instance;
+  }
+
+  PhoneBoat._internal(){
     geolocator = Geolocator();
     int distanceFilter = 10;
     LocationOptions locationOptions = LocationOptions(accuracy: LocationAccuracy.high, distanceFilter: distanceFilter);

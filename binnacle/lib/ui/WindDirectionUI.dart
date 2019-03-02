e5a730facbc019e6d6ad631917d263a6e7186453
Casey Hillers <caseyhillers@gmail.com>
2019-03-02 15:31:53 -0800

    Converted magic numbers to variables

--- binnacle/lib/ui/WindDirectionUI.dart
+++ binnacle/lib/ui/WindDirectionUI.dart
@@ -7,14 +7,15 @@ class WindDirectionUI extends StatelessWidget {
   WindDirectionUI({Key key, this.heading}) : super(key: key);
 
   final double heading;
+  final double radius = 75.0;
+  final Color color = Colors.lightBlue;
 
   Widget build(BuildContext context) {
     return new Transform.rotate(
         angle: ((heading ?? 0) * (math.pi / 180) * -1),
-        origin: Offset(50.0, 50.0),
+        origin: Offset(radius * math.cos((heading ?? 0) * (math.pi / 180)),
+            radius * math.sin(heading ?? 0) * (math.pi / 180)),
         child: CustomPaint(
-            painter: ArrowPainter(color: Colors.green, percentChange: 0.5))
-//        child: (new ArrowPainter(Color.fromRGBO(255, 255, 255, 0), 10)))
-        );
+            painter: ArrowPainter(color: color, percentChange: 0.5)));
   }
 }

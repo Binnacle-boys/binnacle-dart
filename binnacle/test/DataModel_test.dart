4d7a45101c7ba60e02e2d8359f38a4b9e581301d
William Walker <wnwalker@ucsc.edu>
2019-03-03 19:02:19 -0800

    Fixed DataModel test.

--- binnacle/test/DataModel_test.dart
+++ binnacle/test/DataModel_test.dart
@@ -4,14 +4,14 @@ import 'package:sos/model/DataModel.dart';
 
 void main() {
   test('phone data model returned', () {
-    final dm = new DataModel();
+    final dm = new DataModel(SensorType.phone);
 
     expect(dm != null, true);
   });
 
   test('bluetooth data model throws not implemented', () {
     try {
-      final dm = new DataModel();
+      final dm = new DataModel(SensorType.phone);
 
       expect(dm == null, true);
     } catch (e) {

59bf473a5ce882d58d6ec5812b480bd4e4c8a16c
William Walker <wnwalker@ucsc.edu>
2019-03-05 16:01:46 -0800

    Began implementing Bluetooth Serial code
    --Added flutter_bluetooth_serial as a dependency
    --Added code to print bonded bluetooth serial devices

--- binnacle/lib/model/bluetooth/BluetoothManager.dart
+++ binnacle/lib/model/bluetooth/BluetoothManager.dart
@@ -1,13 +1,16 @@
 import 'package:flutter_blue/flutter_blue.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 
 /// A rudimentary class to handle Bluetooth device interactions.
 
 class BluetoothManager{
 
   FlutterBlue flutterBlue;
+  FlutterBluetoothSerial flutterBluetoothSerial;
 
   BluetoothManager(){
     flutterBlue = FlutterBlue.instance;
+    flutterBluetoothSerial = FlutterBluetoothSerial.instance;
   }
 
   /// Print list of Bluetooth devices that are currently nearby and in discovery mode.
@@ -19,6 +22,9 @@ class BluetoothManager{
       if(scanResult != null){
         print(scanResult.device.name + " " + scanResult.device.id.toString());
       }
+    });
+    flutterBluetoothSerial.getBondedDevices().then((list){
+      print("Bonded Serial Devices: "+list.toString());
     });
   }
 

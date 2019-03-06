623515e94d6e2f906b087d0e016e5ee9861c61ea
William Walker <wnwalker@ucsc.edu>
2019-03-05 16:23:43 -0800

    Implemented paired device listing
    --Added code to list previously paired devices, their Bluetooth addresses, and whether or not they are currently connected.

--- binnacle/lib/model/bluetooth/BluetoothManager.dart
+++ binnacle/lib/model/bluetooth/BluetoothManager.dart
@@ -1,30 +1,36 @@
-import 'package:flutter_blue/flutter_blue.dart';
+import 'package:flutter_blue/flutter_blue.dart' as fb;
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 
 /// A rudimentary class to handle Bluetooth device interactions.
 
 class BluetoothManager{
 
-  FlutterBlue flutterBlue;
+  fb.FlutterBlue flutterBlue;
   FlutterBluetoothSerial flutterBluetoothSerial;
 
   BluetoothManager(){
-    flutterBlue = FlutterBlue.instance;
+    flutterBlue = fb.FlutterBlue.instance;
     flutterBluetoothSerial = FlutterBluetoothSerial.instance;
   }
 
   /// Print list of Bluetooth devices that are currently nearby and in discovery mode.
   void printDevices(){
-    flutterBlue.state.then((state){
-      print("Bluetooth state: " + state.toString());
-    });
-    flutterBlue.scan().listen((ScanResult scanResult){
-      if(scanResult != null){
-        print(scanResult.device.name + " " + scanResult.device.id.toString());
-      }
-    });
+//    flutterBlue.state.then((state){
+//      print("Bluetooth state: " + state.toString());
+//    });
+//    flutterBlue.scan().listen((ScanResult scanResult){
+//      if(scanResult != null){
+//        print(scanResult.device.name + " " + scanResult.device.id.toString());
+//      }
+//    });
     flutterBluetoothSerial.getBondedDevices().then((list){
-      print("Bonded Serial Devices: "+list.toString());
+//      print("Bonded Serial Device List: "+list.toString());
+      list.forEach((device){
+        BluetoothDevice btd = device as BluetoothDevice;
+        print("BtDevice: "+btd.name);
+        print("Address: "+btd.address.toString());
+        print("Connected State: "+btd.connected.toString()+"\n");
+      });
     });
   }
 

053432e827253fc7b12a5eb1276a3e042f5db46e
William Walker <wnwalker@ucsc.edu>
2019-03-05 16:01:46 -0800

    Began implementing Bluetooth Serial code
    --Added flutter_bluetooth_serial as a dependency
    --Added code to print bonded bluetooth serial devices

--- binnacle/lib/model/bluetooth/BluetoothManager.dart
+++ binnacle/lib/model/bluetooth/BluetoothManager.dart
@@ -1,25 +1,31 @@
-// import 'package:flutter_blue/flutter_blue.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 
 // /// A rudimentary class to handle Bluetooth device interactions.
 
 // class BluetoothManager{
 
-//   FlutterBlue flutterBlue;
+  FlutterBlue flutterBlue;
+  FlutterBluetoothSerial flutterBluetoothSerial;
 
-//   BluetoothManager(){
-//     flutterBlue = FlutterBlue.instance;
-//   }
+  BluetoothManager(){
+    flutterBlue = FlutterBlue.instance;
+    flutterBluetoothSerial = FlutterBluetoothSerial.instance;
+  }
 
-//   /// Print list of Bluetooth devices that are currently nearby and in discovery mode.
-//   void printDevices(){
-//     flutterBlue.state.then((state){
-//       print("Bluetooth state: " + state.toString());
-//     });
-//     flutterBlue.scan().listen((ScanResult scanResult){
-//       if(scanResult != null){
-//         print(scanResult.device.name + " " + scanResult.device.id.toString());
-//       }
-//     });
-//   }
+  /// Print list of Bluetooth devices that are currently nearby and in discovery mode.
+  void printDevices(){
+    flutterBlue.state.then((state){
+      print("Bluetooth state: " + state.toString());
+    });
+    flutterBlue.scan().listen((ScanResult scanResult){
+      if(scanResult != null){
+        print(scanResult.device.name + " " + scanResult.device.id.toString());
+      }
+    });
+    flutterBluetoothSerial.getBondedDevices().then((list){
+      print("Bonded Serial Devices: "+list.toString());
+    });
+  }
 
 // }

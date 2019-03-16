cd21f5b758d9e1b678d618a2d09dacb4edbd3aaf
nkalscheuer <nkalscheuer@gmail.com>
2019-03-15 18:35:56 -0700

    Made data structure of a plot

--- route_planning/polar_plots/polar_plot.py
+++ route_planning/polar_plots/polar_plot.py
@@ -1,6 +1,21 @@
 import csv
 
 with open('data/test_plot.csv', newline='') as csvfile:
-    plotreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    plotreader = csv.reader(csvfile, delimiter=';', quotechar='|')
+    header = next(plotreader)
+    plot = dict()
     for row in plotreader:
         print(', '.join(row))
+        angle = float(0)
+        for (label, value) in zip(header, row):
+            print(label, ": ", value)
+            if(label == "twa/tws"):
+                print("special label")
+                angle = float(value)
+                plot[angle] = dict()
+            else:
+                print(label, "Not special")
+                plot[angle][label] = float(value)
+    for entry, value in plot.items():
+        print(entry, ": ", value)
+

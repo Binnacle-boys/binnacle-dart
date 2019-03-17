b6a5a568d5c6a9a2077f23be76d2ed7d4f38424d
nkalscheuer <nkalscheuer@gmail.com>
2019-03-16 23:07:59 -0700

    Cleaning up code and adding comments

--- route_planning/polar_plots/polar_plot.py
+++ route_planning/polar_plots/polar_plot.py
@@ -1,35 +1,32 @@
 import csv
 import math
 
+# Used to work with polar plots of sail boats
+# in order to compute optimal headings
 class PolarPlot:
     
     def __init__(self, csv_file):
         self.updatePlot(csv_file)    
     
+    # updates plot with csv file
     def updatePlot(self, csv_file):
         with open(csv_file, newline='') as csvfile:
             plotreader = csv.reader(csvfile, delimiter=';', quotechar='|')
             header = next(plotreader)
             self.plot = dict()
             for row in plotreader:
-                print(', '.join(row))
                 angle = float(0)
                 for (label, value) in zip(header, row):
-                    print(label, ": ", value)
                     if(label == "twa/tws"):
-                        print("special label")
+                        # label of angle, not speed
                         angle = float(value)
                         self.plot[angle] = dict()
                     else:
-                        print(label, "Not special")
                         self.plot[angle][label] = float(value)
-            print('printing plot items')
-            for entry, value in self.plot.items():
-            
-                print(entry, ": ", value)
-        
+    
+    # Gets angle difference preserving sign
+    # Doesn't use modulo in order to preserve negative sign of angle difference
     def __angleDifference(self, firstangle, secondangle):
-        # Doesn't use modulo in order to preserve negative sign of angle difference
         difference = secondangle - firstangle
         while difference < -180:
             difference += 360
@@ -37,30 +34,35 @@ class PolarPlot:
             difference -= 360
         return difference
     
+    # Gets best angle based on relative ideal heading from wind heading
     def __bestAngle(self, ideal_angle, windSpeed):
         windSpeeds = [key for key in next(iter(self.plot.values()))]
-        print("Wind speeds: ", windSpeeds)
-        print('ideal angle', ideal_angle)
+        # Gets closest match for speed
         closestSpeed = min(windSpeeds, key=lambda speed: abs(windSpeed - float(speed)))
-        print("Closest speed: ", closestSpeed)
+        # Checks max cosine of angle difference for ideal and plot entry
         bestAngle = max(self.plot.keys(), key=lambda angle: self.plot[angle][closestSpeed] * math.cos(math.radians(self.__angleDifference(ideal_angle, angle))))
         return bestAngle
     
+    # Finds optimal angle from ideal heading and wind info
+    # Only returns angles in plot
+    # Probably better to simply choose the ideal heading, if only small difference
     def findOptimalAngle(self, wind_heading, wind_speed, ideal_heading):
+        # Plot transform to compute in plot space
         plotideal_angle = self.__angleDifference(ideal_heading, (wind_heading + 180) % 360)
-        print("plot ideal angle: ", plotideal_angle)
         direction = 1
         if (plotideal_angle < 0):
             direction = -1
+        # Gets optimal relative plot angle
         optimal_plot_angle = self.__bestAngle(abs(plotideal_angle), wind_speed)
-        print("Optimal plot angle: ", optimal_plot_angle)
+        # Reverses plot transform
         optimal_angle = (optimal_plot_angle + direction * (wind_heading + 180)) % 360
         return optimal_angle
     
-    
-polar = PolarPlot('data/test_plot.csv')
-angle = polar.findOptimalAngle(0, 8, 180)
-print("Test angle", angle)
+
+# How to use   
+# polar = PolarPlot('data/test_plot.csv')
+# angle = polar.findOptimalAngle(0, 8, 180)
+# print("Test angle", angle)
 
 
     

d2110b4e96e4a168d782e9cd7fa3f9a34828b7c7
William Walker <wnwalker@ucsc.edu>
2019-03-17 14:58:14 -0700

    Requested changes
    --Commented out UI widgets which depend on old fields (need to be refactored)
    --Added ignore comments for imported code which isn't used yet
    --Removed unnecessary Kotlin dependencies (carryover from bluetooth branch)

--- binnacle/lib/main.dart
+++ binnacle/lib/main.dart
@@ -8,8 +8,8 @@ import './CompassWidget.dart';
 
 
 import './SpeedWidget.dart';
-import 'WindRequest.dart';
-import 'ListAngleWidget.dart';
+import 'WindRequest.dart'; //ignore: unused_import
+import 'ListAngleWidget.dart'; //ignore: unused_import
 import './model/bluetooth/BluetoothManager.dart'; //ignore: unused_import
 
 void main() => runApp(MyApp());
@@ -96,53 +96,53 @@ class _MyHomePageState extends State<MyHomePage> {
           // mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             SpeedWidget(positionStream: _model.currentBoat.positionStream),
-            Text(
-              _location == null ? 'Latitude unknown' : 'Latitude: ' + headingFormat.format(_location.latitude),
-              style: Theme.of(context).textTheme.display1,
-            ),
-            ListAngleWidget(listAngleStream: _model.currentBoat.listAngle.stream.asBroadcastStream()),
-            Text(
-              _location == null ? 'Longitude unknown' : 'Longitude: ' + headingFormat.format(_location.longitude),
-              style: Theme.of(context).textTheme.display1,
-            ),
+//            Text(
+//              _location == null ? 'Latitude unknown' : 'Latitude: ' + headingFormat.format(_location.latitude),
+//              style: Theme.of(context).textTheme.display1,
+//            ),
+//            ListAngleWidget(listAngleStream: _model.currentBoat.listAngle.stream.asBroadcastStream()),
+//            Text(
+//              _location == null ? 'Longitude unknown' : 'Longitude: ' + headingFormat.format(_location.longitude),
+//              style: Theme.of(context).textTheme.display1,
+//            ),
             CompassWidget(directionStream: _model.currentBoat.compassHeading?.stream),
-            FutureBuilder<WindRequest>(
-                future: fetchWind(_location),
-                builder: (context, snapshot) {
-                  if(snapshot.connectionState == ConnectionState.done && snapshot.data != null){
-                    return Center(
-                        child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Text(
-                                snapshot.data.wind.heading == null ? 'Wind Heading unknown' : 'Wind Heading: ' + snapshot.data.wind.heading,
-                                style: Theme.of(context).textTheme.display1,
-                              ),
-                              Text(
-                                snapshot.data.wind.speed == null ? 'Wind Speed unknown' : 'Wind Speed: ' + snapshot.data.wind.speed,
-                                style: Theme.of(context).textTheme.display1,
-                              ),
-                            ]
-                        )
-                    );
-                  }
-                  else if(snapshot.hasError){
-                    return Container(
-                      child: Text(snapshot.error.toString())
-                    );
-                  }
-                  else{
-                    return Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          CircularProgressIndicator()
-                        ],
-                      )
-                    );
-                  }
-                }
-            )
+//            FutureBuilder<WindRequest>(
+//                future: fetchWind(_location),
+//                builder: (context, snapshot) {
+//                  if(snapshot.connectionState == ConnectionState.done && snapshot.data != null){
+//                    return Center(
+//                        child: Column(
+//                            mainAxisAlignment: MainAxisAlignment.center,
+//                            children: <Widget>[
+//                              Text(
+//                                snapshot.data.wind.heading == null ? 'Wind Heading unknown' : 'Wind Heading: ' + snapshot.data.wind.heading,
+//                                style: Theme.of(context).textTheme.display1,
+//                              ),
+//                              Text(
+//                                snapshot.data.wind.speed == null ? 'Wind Speed unknown' : 'Wind Speed: ' + snapshot.data.wind.speed,
+//                                style: Theme.of(context).textTheme.display1,
+//                              ),
+//                            ]
+//                        )
+//                    );
+//                  }
+//                  else if(snapshot.hasError){
+//                    return Container(
+//                      child: Text(snapshot.error.toString())
+//                    );
+//                  }
+//                  else{
+//                    return Center(
+//                      child: Column(
+//                        mainAxisAlignment: MainAxisAlignment.center,
+//                        children: <Widget>[
+//                          CircularProgressIndicator()
+//                        ],
+//                      )
+//                    );
+//                  }
+//                }
+//            )
           ],
         ),
       

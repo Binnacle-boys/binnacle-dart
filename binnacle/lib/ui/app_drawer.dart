98524ab67d9312e65d8688f3b26239cbfd18ed60
Donovan Rost <drost@ucsc.edu>
2019-04-09 11:38:11 -0700

    forgot to add this before changing branches

--- binnacle/lib/ui/app_drawer.dart
+++ binnacle/lib/ui/app_drawer.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_picker/flutter_picker.dart';
 
 class AppDrawer extends Drawer {
   Drawer _drawer;
@@ -21,19 +20,6 @@ class AppDrawer extends Drawer {
     );
   }
 
-  showPicker(BuildContext context) {
-      Picker picker = new Picker(
-        adapter: PickerDataAdapter<String>(pickerdata: new JsonDecoder().convert(PickerData)),
-        changeToFirst: true,
-        textAlign: TextAlign.left,
-        columnPadding: const EdgeInsets.all(8.0),
-        onConfirm: (Picker picker, List value) {
-          print(value.toString());
-          print(picker.getSelectedValues());
-        }
-      );
-      picker.show(_scaffoldKey.currentState);
-    }
-
+  
   Drawer get drawer => this._drawer;
 }

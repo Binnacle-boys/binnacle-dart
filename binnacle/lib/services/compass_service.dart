a287667df12cd69a69aa18207ca34dbf85ee07f7
dan-rds <ddrichar@ucsc.edu>
2019-04-13 16:55:01 -0700

    just a few more things

--- binnacle/lib/services/compass_service.dart
+++ binnacle/lib/services/compass_service.dart
@@ -1,26 +1,24 @@
 import 'dart:async';
 import '../models/compass_model.dart';
-import  '../providers/compass_provider.dart';
+import '../providers/compass_provider.dart';
 import 'package:flutter_compass/flutter_compass.dart';
 
 class CompassService extends ICompassService {
-  
   StreamController<CompassModel> _compassStream = StreamController();
 
   CompassService() {
     print('Initializing Compass Service');
-    FlutterCompass.events.listen((data) => print(data.toString()));
-    _compassStream.addStream(FlutterCompass.events.map((double d) => new CompassModel(direction: d)));
+    //FlutterCompass.events.listen((data) => print(data.toString()));
+    _compassStream.addStream(FlutterCompass.events
+        .map((double d) => new CompassModel(direction: d)));
     ; // Do I need to listen here?
   }
   Stream<CompassModel> get compassStream => _compassStream.stream;
-  
+
   dispose() {
     // this._compassStream.stream.drain();
     // this._compassStream.close();
     this._compassStream = null;
     FlutterCompass.events.drain();
-  
   }
-
 }

063565dc5e698a75dd5f8ca0454f24ad0f7d3f97
dan-rds <ddrichar@ucsc.edu>
2019-04-13 16:00:39 -0700

    Added list angle to bloc

--- binnacle/lib/bloc.dart
+++ binnacle/lib/bloc.dart
@@ -1,19 +1,18 @@
 import 'dart:async';
 import 'package:rxdart/rxdart.dart';
+import 'package:sos/models/list_angle_model.dart';
 import './repository.dart';
 import 'models/position_model.dart';
 import 'models/wind_model.dart';
 
-
 class Bloc extends Object {
-
   Repository _repository;
 
   final _headingController = BehaviorSubject<String>();
   final _positionController = BehaviorSubject<PositionModel>();
   final _windContoller = PublishSubject<WindModel>();
   final _compassController = PublishSubject();
-
+  final _listAngleController = PublishSubject<ListAngleModel>();
   //? Should this contructor be refactored in to an aync factory?
   Bloc() {
     // * This line is just a dummy position -- delete it when Position works
@@ -22,19 +21,17 @@ class Bloc extends Object {
     this._positionController.addStream(this._repository.getPositionStream());
     this._windContoller.addStream(_repository.getWindStream());
     this._compassController.addStream(_repository.getCompassStream());
-    
-
+    this._listAngleController.addStream(_repository.getListAngleStream());
   }
 
-
   Stream<WindModel> get wind => _windContoller;
-  PublishSubject get compass => _compassController.stream; 
-  BehaviorSubject<PositionModel> get position => _positionController.stream; // .stream? 
-
-
-    // change data
-    //* These don't actually do anything yet. I'm just leaving them 
-    //* as a reference for whene I need to have functions in BLoC
+  PublishSubject get compass => _compassController.stream;
+  BehaviorSubject<PositionModel> get position =>
+      _positionController.stream; // .stream?
+  PublishSubject<ListAngleModel> get listAngle => _listAngleController.stream;
+  // change data
+  //* These don't actually do anything yet. I'm just leaving them
+  //* as a reference for whene I need to have functions in BLoC
   Function(String) get changeHeading => _headingController.sink.add;
   Function(PositionModel) get changePosition => _positionController.sink.add;
 
@@ -42,7 +39,6 @@ class Bloc extends Object {
     this._repository.swapCompassStream();
   }
 
-  
   void dispose() async {
     await _headingController?.drain();
     await _headingController?.close();
@@ -50,7 +46,7 @@ class Bloc extends Object {
     await _positionController?.close();
     await _windContoller?.drain();
     await _windContoller?.close();
-
+    await _listAngleController?.drain();
+    await _listAngleController?.close();
   }
-
 }

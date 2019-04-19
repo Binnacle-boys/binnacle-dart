70d7b09101048d800414a092c231a20d42cbc650
dan-rds <ddrichar@ucsc.edu>
2019-04-18 22:26:10 -0700

    rm-ing dead code code pt3

--- binnacle/lib/ui/BinnacleHeadingUI.dart
+++ binnacle/lib/ui/BinnacleHeadingUI.dart
@@ -1,8 +1,7 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:sos/models/wind_model.dart';
 import 'package:sos/providers/app_provider.dart';
-
-import 'package:sos/utils/Math.dart';
 import 'ArrowPainter.dart';
 
 class BinnacleHeadingUI extends StatelessWidget {
@@ -15,7 +14,7 @@ class BinnacleHeadingUI extends StatelessWidget {
         builder: (context, AsyncSnapshot<WindModel> snapshot) {
           if (snapshot.hasData) {
             return new Transform.rotate(
-                angle: degreesToRadians(snapshot.data.deg),
+                angle: (pi / 180) * snapshot.data.deg,
                 child: new Align(
                   alignment: Alignment.topCenter,
                   child: CustomPaint(painter: ArrowPainter(percentChange: 0.5)),

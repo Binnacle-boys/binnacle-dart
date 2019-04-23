2f36bf8886164d09ed811470c65304f1300d85d6
Donovan Rost <drost@ucsc.edu>
2019-04-22 19:39:39 -0700

    commented out large block comment in heading_panel_widget

--- binnacle/lib/ui/info_panel/heading_panel_widget.dart
+++ binnacle/lib/ui/info_panel/heading_panel_widget.dart
@@ -51,28 +51,6 @@ class boatHeadingLabel extends StatelessWidget {
   }
 }
 
-/*
-  final bloc = Provider.of(context);
-  final theme = GlobalTheme().get();
-  return StreamBuilder(
-      stream: bloc.compass,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return new Container(
-              alignment: Alignment.center,
-              child: Text(
-                  degreeToCardinalString((snapshot.data.direction.floor())),
-                  style: theme.textTheme.body2));
-        } else if (snapshot.hasError) {
-          print(
-              "Error in heading_panel.dart -> boadHeadingLabel, stream has error");
-          return Text("Error", style: theme.textTheme.body2);
-        } else {
-          return Text("--", style: theme.textTheme.body2);
-        }
-      });
-}
-*/
 String degreeToCardinalString(double heading) {
   // Given a degree, this func returns cardinal arc it's in e.g. 180 => 'S'
   List l = [

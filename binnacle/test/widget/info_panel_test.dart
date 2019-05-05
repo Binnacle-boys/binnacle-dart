194f8f207adb4b7297fe60725d403bc6f131fc06
dan-rds <ddrichar@ucsc.edu>
2019-05-05 14:45:38 -0700

    one file got left behind

--- binnacle/test/widget/info_panel_test.dart
+++ binnacle/test/widget/info_panel_test.dart
@@ -5,14 +5,8 @@ import 'package:sos/ui/global_theme.dart';
 import 'package:sos/ui/info_panel.dart';
 
 void main() {
-  testWidgets(
-      "__Info_panel UI: Proper initializing ui elements with empty streams",
-      (WidgetTester tester) async {
-    await tester.pumpWidget(new Provider(
-        child: MaterialApp(
-            title: 'Binnacle Demo',
-            theme: new GlobalTheme().get(),
-            home: InfoPanel())));
+  testWidgets("__Info_panel UI: Proper initializing ui elements with empty streams", (WidgetTester tester) async {
+    await tester.pumpWidget(new Provider(child: MaterialApp(title: 'Binnacle Demo', theme: new GlobalTheme().toThemeData(), home: InfoPanel())));
     expect(find.text("-.-"), findsNWidgets(2));
     expect(find.text("--"), findsNWidgets(2));
   });

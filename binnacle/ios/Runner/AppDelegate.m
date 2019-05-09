35816c5cddf5820a7d02a67de538acfb3d7c61e9
dan-rds <ddrichar@ucsc.edu>
2019-05-09 16:37:36 -0700

    ios key and imports for map

--- binnacle/ios/Runner/AppDelegate.m
+++ binnacle/ios/Runner/AppDelegate.m
@@ -1,10 +1,13 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
+#import "GoogleMaps/GoogleMaps.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+       [GMSServices provideAPIKey:@"AIzaSyC3ZBTYTiHS4G9G1ZaLzhXhv9zBeCzP2Cs"];
+  
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];

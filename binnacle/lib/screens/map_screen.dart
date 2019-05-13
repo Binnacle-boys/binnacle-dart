47e1b1431f229063a6ada8ffea5766eecce96d04
Casey Hillers <caseyhillers@gmail.com>
2019-05-13 11:37:59 -0700

    add markers, finish course adds lines

--- binnacle/lib/screens/map_screen.dart
+++ binnacle/lib/screens/map_screen.dart
@@ -3,49 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:sos/providers/app_provider.dart';
 
-class MapUI extends StatelessWidget {
-  Completer<GoogleMapController> _controller = Completer();
-  void _onMapCreated(GoogleMapController controller) {
-    _controller.complete(controller);
-  }
-
-  final Set<Marker> _markers = {};
-
-  @override
-  Widget build(BuildContext context) {
-    final bloc = Provider.of(context);
-    return StreamBuilder(
-        stream: bloc.position,
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return Text("Loading...");
-          } else {
-            _markers.add(Marker(
-              // This marker id can be anything that uniquely identifies each marker.
-              markerId: MarkerId("You Are Here".toString()),
-              position: snapshot.data.latlng,
-              infoWindow: InfoWindow(
-                title: 'Mch',
-                snippet: 'Its meh',
-              ),
-              icon: BitmapDescriptor.defaultMarker,
-            ));
-            return Stack(children: [
-              GoogleMap(
-                  onMapCreated: _onMapCreated,
-                  initialCameraPosition: CameraPosition(
-                    target: snapshot.data.latlng,
-                    zoom: 15.0,
-                  ),
-                  markers: _markers,
-                  rotateGesturesEnabled: true,
-                  scrollGesturesEnabled: true,
-                  tiltGesturesEnabled: true),
-            ]);
-          }
-        });
-  }
-}
+const int MAX_MARKERS = 4;
 
 class MapWidget extends StatefulWidget {
   @override
@@ -53,15 +11,129 @@ class MapWidget extends StatefulWidget {
 }
 
 class _MapState extends State<MapWidget> {
+  Map<PolylineId, Polyline> _polylines = <PolylineId, Polyline>{};
+  Completer<GoogleMapController> _controller = Completer();
+  final Set<Marker> _markers = {};
+  bool _isPlacingPoints = false;
+
   @override
   Widget build(BuildContext context) {
+    final bloc = Provider.of(context);
+
+    var _context;
+
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Maps Sample App'),
-            backgroundColor: Colors.green[700],
-          ),
-          body: MapUI()),
-    );
+        home: Scaffold(
+      body: StreamBuilder(
+          stream: bloc.position,
+          builder: (context, snapshot) {
+            _context = context;
+            if (!snapshot.hasData) {
+              return Center(
+                child: new CircularProgressIndicator(),
+              );
+            } else {
+              _markers.add(Marker(
+                // This marker id can be anything that uniquely identifies each marker.
+                markerId: MarkerId("You Are Here".toString()),
+                position: snapshot.data.latlng,
+                infoWindow: InfoWindow(
+                  title: 'Mch',
+                  snippet: 'Its meh',
+                ),
+                icon: BitmapDescriptor.defaultMarker,
+              ));
+              return Stack(children: [
+                GoogleMap(
+                  onMapCreated: _onMapCreated,
+                  onTap: _onMapTapped,
+                  // onTap: (position) {
+                  //   setState(() {
+                  //     _markers.add(Marker(_markers.length.to))
+                  //   });
+                  // }
+                  initialCameraPosition: CameraPosition(
+                    target: snapshot.data.latlng,
+                    zoom: 15.0,
+                  ),
+                  markers: _markers,
+                  polylines: Set<Polyline>.of(_polylines.values),
+                )
+              ]);
+            }
+          }),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _isPlacingPoints = !_isPlacingPoints;
+          });
+
+          if (_isPlacingPoints) {
+            // TODO: This would be nicer if it calls the navigators sliding window
+            Scaffold.of(_context).showSnackBar(new SnackBar(
+              content: new Text("Tap the map for where you want to go"),
+            ));
+          } else {
+            _onCourseFinished();
+          }
+        },
+        child: _isPlacingPoints
+            ? Icon(Icons.assistant_photo, size: 36.0)
+            : Icon(Icons.add_location, size: 36.0),
+        // child: Icon(Icons.assistant_photo)
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    ));
   }
+
+  void _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+  }
+
+  void _onMapTapped(LatLng position) {
+    if (!_isPlacingPoints) {
+      return;
+    }
+
+    if (_markers.length > MAX_MARKERS) {
+      // TODO: Use the navigator sliding window
+      Scaffold.of(context).showSnackBar(new SnackBar(
+        content: new Text("Max number of markers added"),
+      ));
+    }
+
+    // TODO: Use a custom icon instead of the default Google Map one
+    Marker marker = new Marker(
+        markerId: MarkerId(_markers.length.toString()),
+        position: position,
+        icon: BitmapDescriptor.defaultMarker);
+
+    setState(() {
+      _markers.add(marker);
+    });
+  }
+
+  void _onCourseFinished() {
+    List<Marker> markers = _markers.toList();
+    for (int i = 0; i < markers.length - 1; i++) {
+      Marker from = markers.elementAt(i);
+      Marker to = markers.elementAt(i + 1);
+
+      PolylineId lineId = PolylineId(i.toString());
+      Polyline line = Polyline(
+        polylineId: lineId,
+        color: Colors.red,
+        width: 5,
+        points: [from.position, to.position]
+      );
+
+      setState(() {
+        _polylines[lineId] = line;
+      });
+      print('added $lineId');
+    }
+
+    print('finished course');
+  }
+
 }

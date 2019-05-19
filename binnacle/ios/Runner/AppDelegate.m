96dd3e5f46e8a23390bc88381b5a3c39a69e2883
Daniel Richards <ddrichar@ucsc.edu>
2019-05-18 17:45:54 -0700

    Update AppDelegate.m
    Correcting api key retrieval for iOS

--- binnacle/ios/Runner/AppDelegate.m
+++ binnacle/ios/Runner/AppDelegate.m
@@ -2,15 +2,17 @@
 #include "GeneratedPluginRegistrant.h"
 #import "GoogleMaps/GoogleMaps.h"
 
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-   
+
     // Provide the GoogleMaps API key.
-    NSString* mapsApiKey = [[NSProcessInfo processInfo] environment][@"OST_MAPS_API_KEY"];
-    [GMSServices provideAPIKey:mapsApiKey];
-  
+   NSString *path = [[NSBundle mainBundle] pathForResource: @"keys" ofType: @"plist"];
+  NSDictionary *dict = [NSDictionary dictionaryWithContentsOfFile: path];
+  id obj = [dict objectForKey: @"maps_api_key"];
+  [GMSServices provideAPIKey:obj];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];

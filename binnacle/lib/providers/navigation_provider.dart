a2cc35a71573a40caed3af93454151a210281fb8
dan-rds <ddrichar@ucsc.edu>
2019-05-19 16:01:54 -0700

    Heading ui, Ideal heading ui and I put the List angle ui at the bottom

--- binnacle/lib/providers/navigation_provider.dart
+++ binnacle/lib/providers/navigation_provider.dart
@@ -2,37 +2,32 @@ import 'dart:async';
 
 import 'package:flutter/foundation.dart';
 import 'package:rxdart/rxdart.dart';
-import 'package:sail_routing_dart/cart_point.dart';
 import 'package:sail_routing_dart/route_model.dart';
 import 'package:sos/models/position_model.dart';
 import 'package:sos/enums.dart';
+import 'package:vector_math/vector_math.dart';
 
 class NavigationProvider {
   Stream _position;
   Stream _wind;
-  CartPoint _start;
-  CartPoint _end;
+  Vector2 _start;
+  Vector2 _end;
   RouteModel _route;
   BehaviorSubject eventBus = BehaviorSubject();
-
 
   NavigationProvider({@required position, @required wind}) {
     _position = position;
     _wind = wind;
-    eventBus.add(NavigationEvent(eventType: NavigationEventType.awaitingInit ));
+    eventBus.add(NavigationEvent(eventType: NavigationEventType.awaitingInit));
   }
   initNavigation() {
     print('!!! FROM INITNAV');
     eventBus.add(NavigationEvent(eventType: NavigationEventType.init));
-
-
   }
-
 
   // start(PositionModel start, PositionModel end) {
   //   _start = _positionToCartPoint(start);
   //   _end = _positionToCartPoint(end);
-
 
   // }
   // RouteModel calculateRoute(start, end) {
@@ -42,11 +37,9 @@ class NavigationProvider {
   // _positionToCartPoint(p) => CartPoint(p.lat, p.lon);
   // _cartPointToPosition(c) => PositionModel(lat: c.x, lon: c.y, speed: 0.0);
 
-
 }
 
 class NavigationEvent {
   final NavigationEventType eventType;
   NavigationEvent({@required this.eventType});
-  
 }
--- binnacle/lib/ui/ideal_heading_widget.dart
+++ binnacle/lib/ui/ideal_heading_widget.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:sos/models/compass_model.dart';
 import 'package:sos/ui/global_theme.dart';
 import '../bloc.dart';
 import '../providers/app_provider.dart';
@@ -9,61 +10,54 @@ import '../providers/app_provider.dart';
 class IdealHeadingUI extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.bottomCenter,
-      child: IdeaHeadingStreamBuilder(context),
-    );
+    Bloc bloc = Provider.of(context);
+    return StreamBuilder(
+        stream: bloc.compass,
+        builder: (context, AsyncSnapshot<CompassModel> snapshot) {
+          if (snapshot.hasData) {
+            // return CircularProgressIndicator();
+            return Stack(children: [
+              new Transform.rotate(
+                  //TODO just hooked up to heading stream for now bc ideal heading doesnt exist
+                  angle: (snapshot.data.direction * pi / 180),
+                  child: new Align(
+                    alignment: Alignment.center,
+                    child: CustomPaint(painter: IdealHeadingArrowPainter(superContext: context)),
+                  )),
+              new Container(alignment: Alignment.center, child: CustomPaint(painter: HeadingArrowPainter(superContext: context))),
+            ]);
+          } else if (snapshot.hasError) {
+            print("Stream error in list_angle_widget.dart -> listAngleStreamBuilder");
+            return Text(' ');
+          } else {
+            return CircularProgressIndicator();
+          }
+        });
   }
-}
-
-Widget IdeaHeadingStreamBuilder(BuildContext context) {
-  Bloc bloc = Provider.of(context);
-  return StreamBuilder(
-      stream: bloc.listAngle,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          // return CircularProgressIndicator();
-          return Stack(children: [
-            new Center(
-                child: Container(
-                    height: 250.0, width: 250.0, child: new CustomPaint(foregroundPainter: new IdealHeadingArrowPainter(lineColor: Colors.green)))),
-            new Center(
-                child: Container(
-                    height: 250.0,
-                    width: 250.0,
-                    child: new CustomPaint(foregroundPainter: new HeadingArrowPainter(lineColor: GlobalTheme().listAngleColor)))),
-          ]);
-        } else if (snapshot.hasError) {
-          print("Stream error in list_angle_widget.dart -> listAngleStreamBuilder");
-          return Text(' ');
-        } else {
-          return CircularProgressIndicator();
-        }
-      });
 }
 
 class HeadingArrowPainter extends CustomPainter {
   Color lineColor;
 
   double width = 4;
+  BuildContext superContext;
+  var w, h;
 
-  HeadingArrowPainter({this.lineColor});
+  HeadingArrowPainter({this.superContext});
+
   @override
   void paint(Canvas canvas, Size size) {
     Paint complete = new Paint()
-      ..color = lineColor
+      ..color = GlobalTheme().primaryColor
       ..strokeCap = StrokeCap.butt
       ..style = PaintingStyle.stroke
       ..strokeWidth = width;
     Path path = new Path();
-
-    // path.moveTo(size.width / 2, size.height);
-    // path.lineTo(size.width / 2, 0);
-    // path.addPolygon([Offset(size.width / 2 + 5, 5), Offset(size.width / 2 - 5, 5), Offset(size.width / 2, -5)], true);
-    path.moveTo(size.width / 2, size.height);
-    path.lineTo(size.width / 2, 0);
-    path.addPolygon([Offset(size.width / 2 + 5, 5), Offset(size.width / 2 - 5, 5), Offset(size.width / 2, -5)], true);
-
+    w = superContext.size.width;
+    h = superContext.size.height;
+    path.moveTo(0, -130);
+    path.lineTo(0, 140);
+    path.addPolygon([Offset(3, -130), Offset(0, -135), Offset(-3, -130)], true);
     path.close();
     canvas.drawPath(path, complete);
   }
@@ -77,28 +71,46 @@ class HeadingArrowPainter extends CustomPainter {
 class IdealHeadingArrowPainter extends CustomPainter {
   Color lineColor;
 
-  double width = 30;
+  double width = 10;
+  BuildContext superContext;
 
-  IdealHeadingArrowPainter({this.lineColor});
+  IdealHeadingArrowPainter({this.superContext});
   @override
   void paint(Canvas canvas, Size size) {
+    var theme = GlobalTheme();
     Paint complete = new Paint()
-      ..color = lineColor
+      ..color = theme.backgroundColor
       ..strokeCap = StrokeCap.butt
-      ..style = PaintingStyle.fill
+      ..style = PaintingStyle.stroke
       ..strokeWidth = width;
     Path path = new Path();
+    List<List<double>> tuples = [
+      [-2, 25],
+      [5, 25],
+      [5, 250],
+      [9, 250],
+      [10, 300],
+      [17, 300],
+      [17, 250],
+      [21, 250],
+      [20, 25],
+      [27, 25],
+      [13, 0],
+    ];
 
-    // path.moveTo(size.width / 2, size.height);
-    // path.lineTo(size.width / 2, 0);
-    // path.addPolygon([Offset(size.width / 2 + 5, 5), Offset(size.width / 2 - 5, 5), Offset(size.width / 2, -5)], true);
-    Rect rect = new Rect.fromPoints(Offset(size.width / 2, size.height), Offset(size.width / 2, 15));
-    //path.moveTo(size.width / 2, size.height);
-    //path.lineTo(size.width / 2, 0);
-    // path.addPolygon([Offset(size.width / 2 + 5, 5), Offset(size.width / 2 - 5, 5), Offset(size.width / 2, -5)], true);
-    // path.addRect(rect);
+    List<Offset> l = tuples.map((t) => Offset(t[0] - 13, t[1] - 150)).toList();
+    path.addPolygon(l, true);
+
     path.close();
-    canvas.drawRect(rect, complete);
+    //path = path.shift(Offset(-92, -250));
+    canvas.drawPath(path, complete);
+
+    complete.color = theme.primaryColor;
+    complete.strokeWidth = 3;
+    canvas.drawPath(path, complete);
+
+    complete.color = theme.backgroundColor;
+    complete.style = PaintingStyle.fill;
     canvas.drawPath(path, complete);
   }
 
